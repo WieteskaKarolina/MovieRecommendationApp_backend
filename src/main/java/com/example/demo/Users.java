cdcd9785ee1a2adf15e10bc788adc69625f20935
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-10-22 19:12:23 +0200

    Create repository
    Add User class

---
+++ src/main/java/com/example/demo/Users.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long user_id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
